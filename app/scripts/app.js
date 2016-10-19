20fe6af8a73ca19902dd526b9f8a6d713b026106
Manpreet "Mannie" Singh <raevox@gmail.com>
2016-10-19 11:47:39 -0400

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,10 @@
+global.jQuery = require('jquery');
+global.$ = global.jQuery;
+global.Tether = require('tether');
+require('bootstrap');
+
+function init() {
+  // TODO: Content goes here
+}
+
+$(document).ready(init);
